47fb408c8b9bc082b22956772a539ef0807fdabd
Fares Kobbi <fares@MSI>
2024-10-11 15:58:29 +0200

    init card class (constructor and getter)

--- src/ps5/Card.java
+++ src/ps5/Card.java
@@ -1,4 +1,20 @@
 package ps5;
 
 public class Card {
+    private final String cardColor;
+    private final String cardSuit;
+
+    public Card(String cardColor, String cardSuit) {
+        this.cardColor = cardColor;
+        this.cardSuit = cardSuit;
+    }
+
+    public String getCardColor() {
+        return cardColor;
+    }
+
+    public String getCardSuit() {
+        return cardSuit;
+    }
+
 }
