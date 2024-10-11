b49d057131c1b55b5e8dccb4ea9b19b3481a161d
youssefBM2104 <mzoughia2104@gmail.com>
2024-10-11 16:50:57 +0200

    init class Hand

--- src/ps5/Hand.java
+++ src/ps5/Hand.java
@@ -1,4 +1,20 @@
 package ps5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+    private List<Card> cardList = new ArrayList<>();
+
+    public Hand(List<Card> cardList) {
+        this.cardList = cardList;
+    }
+
+    public List<Card> getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(List<Card> cardList) {
+        this.cardList = cardList;
+    }
 }
