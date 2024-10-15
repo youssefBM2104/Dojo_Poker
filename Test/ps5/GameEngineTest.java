50abbe8b49624631873351b7f40fd6894d4dc833
Fares Kobbi <fares@MSI>
2024-10-15 15:20:40 +0200

    isBrelan and isFullColor tested

--- Test/ps5/GameEngineTest.java
+++ Test/ps5/GameEngineTest.java
@@ -3,6 +3,9 @@ package ps5;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameEngineTest {
@@ -13,59 +16,82 @@ class GameEngineTest {
 
     @BeforeEach
     void setup() {
-
-
+        handScanner = new HandScanner();
+        hand = new Hand();
+        gameEngine = new GameEngine();
     }
 
     @Test
-    void isFullColor1() {
-        handScanner = new HandScanner();
-        hand = new Hand();
-
+    void testIsFullColor() {
         hand.addCardToHand(handScanner.getCardFromString("10Tr"));
         hand.addCardToHand(handScanner.getCardFromString("ACa"));
         hand.addCardToHand(handScanner.getCardFromString("7Co"));
         hand.addCardToHand(handScanner.getCardFromString("RPi"));
         hand.addCardToHand(handScanner.getCardFromString("2Co"));
 
+        assertFalse(gameEngine.isFullColor(hand));
 
 
-    }
-
-    @Test
-    void isFullColor2() {
-        handScanner = new HandScanner();
         hand = new Hand();
-
         hand.addCardToHand(handScanner.getCardFromString("10Tr"));
         hand.addCardToHand(handScanner.getCardFromString("ATr"));
         hand.addCardToHand(handScanner.getCardFromString("7Tr"));
         hand.addCardToHand(handScanner.getCardFromString("RTr"));
         hand.addCardToHand(handScanner.getCardFromString("2Co"));
-    }
 
-    @Test
-    void isFullColor3() {
-        handScanner = new HandScanner();
+        assertFalse(gameEngine.isFullColor(hand));
+
+
         hand = new Hand();
-
         hand.addCardToHand(handScanner.getCardFromString("10Tr"));
         hand.addCardToHand(handScanner.getCardFromString("ATr"));
         hand.addCardToHand(handScanner.getCardFromString("7Tr"));
         hand.addCardToHand(handScanner.getCardFromString("RTr"));
         hand.addCardToHand(handScanner.getCardFromString("2Tr"));
-    }
 
-    @Test
-    void isFullColor4() {
-        handScanner = new HandScanner();
+        assertTrue(gameEngine.isFullColor(hand));
+
+
         hand = new Hand();
-
         hand.addCardToHand(handScanner.getCardFromString("10Tr"));
         hand.addCardToHand(handScanner.getCardFromString("ACa"));
         hand.addCardToHand(handScanner.getCardFromString("7Ca"));
         hand.addCardToHand(handScanner.getCardFromString("RCa"));
         hand.addCardToHand(handScanner.getCardFromString("2Ca"));
+
+        assertFalse(gameEngine.isFullColor(hand));
+    }
+
+
+    @Test
+    void testIsBrelan(){
+        String input = "10Tr 10Ca 7Co 10Pi 2Co";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        handScanner.handScan(hand, 1);
+
+        assertTrue(gameEngine.isBrelan(hand));
+
+
+        hand = new Hand();
+
+        input = "10Tr 8Ca 7Co 10Pi 2Co";
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        handScanner.handScan(hand, 1);
+
+        assertFalse(gameEngine.isBrelan(hand));
+
+
+        hand = new Hand();
+
+        input = "10Tr 10Ca";
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        handScanner.handScan(hand, 1);
+
+        assertFalse(gameEngine.isBrelan(hand));
+
     }
 
 }
