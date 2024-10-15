cd0ee75ea4e48d9de8b8218ec299985bd58c42bb
youssefBM2104 <mzoughia2104@gmail.com>
2024-10-15 14:26:43 +0200

    isBrelan implemented but not tested

--- Test/ps5/GameEngineTest.java
+++ Test/ps5/GameEngineTest.java
@@ -1,4 +1,25 @@
+package ps5;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class GameEngineTest {
-  
+
+    private HandScanner handScanner;
+    private Hand hand;
+    private GameEngine gameEngine;
+
+    @BeforeEach
+    void setup(){
+        hand = new Hand();
+        handScanner = new HandScanner();
+        gameEngine = new GameEngine();
+    }
+
+
+    @Test
+    void isBrelan() {
+
+    }
 }
--- src/ps5/GameEngine.java
+++ src/ps5/GameEngine.java
@@ -1,8 +1,29 @@
 package ps5;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameEngine {
     public boolean isBrelan(Hand hand){
+        Map<String,Integer> hashmap = new HashMap<>();
+
+        for (Card card : hand.getCardList()){
+            if (hashmap.containsKey(card.getCardValue())){
+                hashmap.put(card.getCardValue(), hashmap.get(card.getCardValue())+1);
+            }
+            else{
+                hashmap.put(card.getCardValue(),1);
+            }
+        }
+
+        for (Integer n: hashmap.values()){
+            if (n==3){
+                return true;
+            }
+        }
         return false;
+
     }
     public boolean isFullColor(Hand hand){
         return false;
