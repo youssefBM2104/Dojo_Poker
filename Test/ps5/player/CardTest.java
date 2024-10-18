5bedfb2e7a18e170162edd73b520aa05861619f6
Fares Kobbi <fares@MSI>
2024-10-18 17:07:19 +0200

    deleted useless test

--- Test/ps5/player/CardTest.java
+++ Test/ps5/player/CardTest.java
@@ -7,21 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    @org.junit.jupiter.api.Test
-    void testConvStringValueToInt() {
-        Card card1 = new Card(CardColor.TR, CardValue.R);
-        Card card2 = new Card(CardColor.TR, CardValue.A);
-        Card card3 = new Card(CardColor.TR, CardValue.SEVEN);
-        Card card4 = new Card(CardColor.TR, CardValue.D);
-        Card card5 = new Card(CardColor.TR, CardValue.V);
-
-        assertEquals(11, card1.getCardValue());
-        assertEquals(12, card2.getCardValue());
-        assertEquals(5, card3.getCardValue());
-        assertEquals(10, card4.getCardValue());
-        assertEquals(9, card5.getCardValue());
-
-    }
 
     @org.junit.jupiter.api.Test
     void testComparaison(){
