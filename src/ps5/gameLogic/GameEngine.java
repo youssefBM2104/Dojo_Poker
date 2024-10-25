8266910de7969738d61ef4c6115e7b80abe7900d
Fares Kobbi <fares@MSI>
2024-10-25 16:17:02 +0200

    whoWon implemented

--- src/ps5/gameLogic/GameEngine.java
+++ src/ps5/gameLogic/GameEngine.java
@@ -4,6 +4,7 @@ package ps5.gameLogic;
 import ps5.player.Card;
 import ps5.player.Hand;
 import ps5.player.HandInterface;
+import ps5.player.enums.HandId;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -26,6 +27,25 @@ public class GameEngine {
                 break;
             }
         }
+    }
 
+    public HandId whoWon(Hand hand1, Hand hand2){
+        if (hand1.getHandPriority().ordinal() > hand2.getHandPriority().ordinal()){
+            return HandId.HAND_1;
+        }
+        else if(hand1.getHandPriority().ordinal() < hand2.getHandPriority().ordinal()){
+            return HandId.HAND_2;
+        }
+        else{
+            if(hand1.getHighestCard().ordinal() > hand2.getHighestCard().ordinal()){
+                return HandId.HAND_1;
+            }
+            else if(hand1.getHighestCard().ordinal() < hand2.getHighestCard().ordinal()){
+                return HandId.HAND_2;
+            }
+            else{
+                return HandId.DEFAULT;
+            }
+        }
     }
 }
---
+++ src/ps5/player/enums/HandId.java
@@ -0,0 +1,7 @@
+package ps5.player.enums;
+
+public enum HandId {
+    HAND_1,
+    HAND_2,
+    DEFAULT
+}
