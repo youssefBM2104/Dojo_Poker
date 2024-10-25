a2641d8eebc71c43dc481b41e4bc699d6411f8e0
youssefBM2104 <mzoughia2104@gmail.com>
2024-10-25 16:01:55 +0200

    runAllPossibleHands implemented not tested

--- src/ps5/gameLogic/GameEngine.java
+++ src/ps5/gameLogic/GameEngine.java
@@ -3,7 +3,9 @@ package ps5.gameLogic;
 
 import ps5.player.Card;
 import ps5.player.Hand;
+import ps5.player.HandInterface;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,4 +13,19 @@ import java.util.List;
 
 public class GameEngine {
 
+    public void runAllPossibleHands(Hand hand){
+        boolean result = false;
+        for (Method method : HandInterface.class.getMethods()){
+            try {
+                result = (Boolean) method.invoke(hand);
+            }catch (Exception e){
+                System.out.println("This should never happen");
+                return;
+            }
+            if (result){
+                break;
+            }
+        }
+
+    }
 }
